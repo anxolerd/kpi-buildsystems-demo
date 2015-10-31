ff8667b535c50db47a8fd46ba5b76c384883c949
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-10-31 22:07:40 +0200

    [gulp] rename files

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,14 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('scripts', [], function() {
     gulp.src('src/js/**/*.js')
         .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('build/js'));
 });
 
